66e745313313c1f12a6a224b3bfee8f8b4ece18b
Peter Kim <peter-minho-kim@gmail.com>
2018-12-05 15:07:31 -0500

    Adds mock CC name and number changes

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -5,18 +5,38 @@ class Payment extends React.Component {
     super()
 
     this.state = {
-      ccInputOne: '0000',
-      ccInputTwo: '1234',
-      ccInputThree: '5678',
-      ccInputFour: '9123',
+      ccInputOne: '',
+      ccInputTwo: '',
+      ccInputThree: '',
+      ccInputFour: '',
       ccName: '',
       ccDate: '01/19'
     }
     this.onNameChange = this.onNameChange.bind(this)
+    this.onCCOneChange = this.onCCOneChange.bind(this)
+    this.onCCTwoChange = this.onCCTwoChange.bind(this)
+    this.onCCThreeChange = this.onCCThreeChange.bind(this)
+    this.onCCFourChange = this.onCCFourChange.bind(this)
   }
   onNameChange(e) {
     const name = e.target.value
     this.setState(() => ({ ccName: name }))
+  }
+  onCCOneChange(e) {
+    const numbers = e.target.value
+    this.setState(() => ({ ccInputOne: numbers }))
+  }
+  onCCTwoChange(e) {
+    const numbers = e.target.value
+    this.setState(() => ({ ccInputTwo: numbers }))
+  }
+  onCCThreeChange(e) {
+    const numbers = e.target.value
+    this.setState(() => ({ ccInputThree: numbers }))
+  }
+  onCCFourChange(e) {
+    const numbers = e.target.value
+    this.setState(() => ({ ccInputFour: numbers }))
   }
   render() {
     return (
@@ -31,10 +51,10 @@ class Payment extends React.Component {
               visa
               </p>
             <div className="mock-cc-numbers u-margin-bottom-s-m">
-              <span className="mock-cc-no mock-cc-no--1">{this.state.ccInputOne}</span>
-              <span className="mock-cc-no mock-cc-no--2">{this.state.ccInputTwo}</span>
-              <span className="mock-cc-no mock-cc-no--3">{this.state.ccInputThree}</span>
-              <span className="mock-cc-no mock-cc-no--4">{this.state.ccInputFour}</span>
+              <span className="mock-cc-no mock-cc-no--1">{this.state.ccInputOne ? this.state.ccInputOne : '0000'}</span>
+              <span className="mock-cc-no mock-cc-no--2">{this.state.ccInputTwo ? this.state.ccInputTwo : '1234'}</span>
+              <span className="mock-cc-no mock-cc-no--3">{this.state.ccInputThree ? this.state.ccInputThree : '5678'}</span>
+              <span className="mock-cc-no mock-cc-no--4">{this.state.ccInputFour ? this.state.ccInputFour : '9123'}</span>
             </div>
             <div className="mock-name-exp">
               <p className="mock-name-exp__name" ref="mockName">{this.state.ccName ? this.state.ccName : 'Your Name'}</p>
@@ -57,10 +77,34 @@ class Payment extends React.Component {
         <div className="input-group">
           <label className="payment-info__label" htmlFor="cc-number-1">Card Number</label>
           <div className="payment__cc-no-box">
-            <input type="text" className="payment-info payment-info__number u-margin-bottom-m" id="cc-number-1" placeholder="0000" />
-            <input type="text" className="payment-info payment-info__number u-margin-bottom-m" id="cc-number-2" placeholder="1234" />
-            <input type="text" className="payment-info payment-info__number u-margin-bottom-m" id="cc-number-3" placeholder="5678" />
-            <input type="text" className="payment-info payment-info__number u-margin-bottom-m" id="cc-number-4" placeholder="9123" />
+            <input 
+              type="text" 
+              className="payment-info payment-info__number u-margin-bottom-m" 
+              id="cc-number-1" 
+              placeholder="0000" 
+              onChange={this.onCCOneChange}
+            />
+            <input 
+              type="text" 
+              className="payment-info payment-info__number u-margin-bottom-m" 
+              id="cc-number-2" 
+              placeholder="1234" 
+              onChange={this.onCCTwoChange}
+            />
+            <input 
+              type="text" 
+              className="payment-info payment-info__number u-margin-bottom-m" 
+              id="cc-number-3" 
+              placeholder="5678" 
+              onChange={this.onCCThreeChange}
+            />
+            <input 
+              type="text" 
+              className="payment-info payment-info__number u-margin-bottom-m" 
+              id="cc-number-4" 
+              placeholder="9123" 
+              onChange={this.onCCFourChange}
+            />
           </div>
         </div>
 
